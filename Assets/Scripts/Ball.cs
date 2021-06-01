88ac9eae250987a459972fd84c1cd19a458cc9fc
Gossa Hernan <hernanagustin.g24@hotmail.com>
2021-06-01 18:54:30 -0300

    Player(30:32 minutes)

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public delegate void BallDie();
+    public BallDie ballDie;
+    [SerializeField] Player player;
+    public bool isMoving { get; set; }
+    void Start()
+    {
+        player.ballController += SwitchState;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void SwitchState(){
+        isMoving = !isMoving;
+    }
+
+    void OnDisable(){
+        player.ballController -= SwitchState;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float movementVelocity = 20f;
+    Vector3 newPosition;
+    [SerializeField] int lifes = 3;
+    [SerializeField] Ball ball;
+    public delegate void ImDie();
+    public ImDie playerDie;
+    public delegate void BallController();
+    public BallController ballController;
+    void Start()
+    {
+        ball.ballDie += IsBallDie;
+    }
+
+    void Update()
+    {
+        Movement();
+        FreeBall();
+    }
+
+    void Movement(){
+        float zAxis = Input.GetAxis("Horizontal");
+        newPosition = new Vector3(zAxis * movementVelocity * Time.deltaTime, 0f, 0f);
+        transform.position += newPosition;
+    }
+
+    void FreeBall(){
+        if(!ball.isMoving){
+            if(Input.GetKeyDown(KeyCode.Space)){
+                ballController();
+            }
+        }
+    }
+
+    void IsBallDie(){
+        lifes--;
+        if(lifes <= 0){
+            playerDie();
+        }
+    }
+
+    void OnDisable(){
+        ball.ballDie -= IsBallDie;
+    }
+}
